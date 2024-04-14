fc4739fd9066b632e6e9a470bd8535e8ccfaf79a
Daiana de Paula <daianaadepaula1@gmail.com>
2024-04-14 17:44:20 -0300

    feat: summary and tags

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import fastify from "fastify";
 import fastifySwagger from "@fastify/swagger";
 import fastifySwaggerUI from "@fastify/swagger-ui";
 
-import { serializerCompiler, validatorCompiler } from "fastify-type-provider-zod";
+import { serializerCompiler, validatorCompiler, jsonSchemaTransform } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
@@ -14,7 +14,20 @@ import { getEventAttendees } from "./routes/get-event-attendees";
 const app = fastify()
 
 app.register(fastifySwagger, {
-	
+	swagger: {
+		consumes: ['application/json'],
+		produces: ['application/json'],
+		info: {
+			title: 'pass.in',
+			description: 'Especificações da API para o back-end da aplicação pass.in construida durante o NLW Unite da Rocketseat',
+			version: '1.0.0'
+		},
+	},
+	transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+	routePrefix: '/docs',
 })
 
 app.setValidatorCompiler(validatorCompiler);
