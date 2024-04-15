30eb56158b5525e8c08988e5aa39f3471890251d
Daiana de Paula <daianaadepaula1@gmail.com>
2024-04-15 09:39:53 -0300

    feat: Correction of errors

--- src/routes/create-event.ts
+++ src/routes/create-event.ts
@@ -3,6 +3,7 @@ import { z } from "zod"
 import { generateSlug } from "../utils/generate-slug"
 import { prisma } from "../lib/prisma"
 import { FastifyInstance } from "fastify"
+import { BadRequest } from "./_erros/bad-request"
 
 export async function createEvent(app: FastifyInstance){
 	app
@@ -12,7 +13,9 @@ export async function createEvent(app: FastifyInstance){
 				summary: 'Create an event',
 				tags: ['events'],
 				body: z.object({
-					title: z.string().min(4),
+					title: z.string({ invalid_type_error: 'The title must be text!' }).min(4, {
+						message: 'Minimum character limit not met! Please ensure your input contains at least 4 characters.',
+					}),
 					details: z.string().nullable(),
 					maximumAttendees: z.number().int().positive().nullable(),
 				}),
@@ -40,7 +43,7 @@ export async function createEvent(app: FastifyInstance){
 			})
 
 			if(eventWithSameSlug !== null){
-				throw new Error('Another event with same title already exists.')
+				throw new BadRequest('Another event with same title already exists.')
 			}
 
 			const event = await prisma.event.create({
